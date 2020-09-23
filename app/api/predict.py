6f8ad076f91bcec43220496083b9eaee654a3ffe
timrocar <timrocar@gmail.com>
2020-09-23 18:09:52 -0400

    bug fixing

--- app/api/predict.py
+++ app/api/predict.py
@@ -61,25 +61,3 @@ async def predict(property: Property):
     price = np.exp(prediction[0]) 
     return '{}$ per night is an optimal price.'.format(round(price))
 
-
-@router.get('/get/{predict}')
-async def get_predict(property: Property):
-    """
-    Predict AirBnB rental prices using app data and home features.
-
-    ### Request body
-    - beds: int
-    - bathrooms: float
-    - bedrooms: float 
-    - accommodates: int
-    - room_type: str
-    - cancellation_policy: str
-    - property_type: str
-
-
-    ### Response 
-    '{prediction}$ per night is an optimal price.' 
-"""
-    prediction = model.predict(property.to_df())
-    price = np.exp(prediction[0]) 
-    return '{}$ per night is an optimal price.'.format(round(price))
