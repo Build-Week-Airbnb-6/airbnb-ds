84c70a5f2ff5371f717e74004908dcd29d3e6c07
timrocar <timrocar@gmail.com>
2020-09-23 12:45:15 -0400

    Redeploy to Heroku

--- app/api/viz.py
+++ app/api/viz.py
@@ -1,21 +1,30 @@
+import numpy as np
 from fastapi import APIRouter, HTTPException
-import pandas as pd
-import plotly.express as px
-import random
-router = APIRouter()
+
+import joblib
+from predict import Property
+
+#router = APIRouter()
+
+#model = joblib.load("app/api/adaboost.joblib")
 
 
-@router.get('/viz/{string}')
-async def viz(placeholder: str):
-    """
-    placeholder
-    
-    ### Path Parameter
-    placeholder : number
+#@router.get('/get/predict')
+#async def predict(property: Property):
+"""
+    Predict AirBnB rental prices using app data and home features.
+    ### Request body
+    - beds: int
+    - bathrooms: float
+    - bedrooms: float 
+    - accommodates: int
+    - room_type: str
+    - cancellation_policy: str
+    - property_type: str
 
-    ### Response
-    placeholder string
-    """
-    string = '{}$ per night is an optimal price.'.format(random.randrange(50, 550, 10))
-    # Return Plotly figure as JSON string
-    return string
+
+    ### Response 
+    '{prediction}$ per night is an optimal price.' 
+"""
+    #prediction = model.predict(property.to_df())
+    #price = np.exp(prediction[0]) 
